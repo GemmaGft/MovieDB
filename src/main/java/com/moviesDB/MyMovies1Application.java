fe35d3f1276553ccfa22ee0a8b49a3fa5e83b506
GemmaGft <gamr@gft.com>
2022-09-07 09:55:55 +0000

    'initial'

---
+++ src/main/java/com/moviesDB/MyMovies1Application.java
@@ -0,0 +1,13 @@
+package com.moviesDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMovies1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMovies1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviesDB/MyMovies1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovies1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
