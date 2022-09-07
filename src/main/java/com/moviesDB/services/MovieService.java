42f24a7602e7cab89d22ed49a94593f9bc3c0665
GemmaGft <gamr@gft.com>
2022-09-07 14:54:49 +0000

    'up'

--- src/main/java/com/moviesDB/services/MovieService.java
+++ src/main/java/com/moviesDB/services/MovieService.java
@@ -3,10 +3,14 @@ package com.moviesDB.services;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.moviesDB.entities.UserMovie;
+
 @Service
+@SuppressWarnings("unchecked")
 public class MovieService {
 
 	WebClient webClient = WebClient.create("https://api.themoviedb.org/3/");
@@ -25,7 +29,6 @@ public class MovieService {
 				.uri(uriBuilder -> uriBuilder.path("movie/popular").queryParam("api_key", api_key).build())
 				.retrieve().bodyToMono(HashMap.class).block();
 	}
-
 	public HashMap<String, Object> findMovieById(int id) {
 		return webClient.get()
 				.uri(uriBuilder -> uriBuilder.path("movie/" + id).queryParam("api_key", api_key).build())
@@ -37,31 +40,26 @@ public class MovieService {
 				.uri(uriBuilder -> uriBuilder.path("movie/top_rated" ).queryParam("api_key", api_key).build())
 				.retrieve().bodyToMono(HashMap.class).block();
 	}
-
 	public HashMap<String, Object> findCredits(int id) {
 		return webClient.get()
 				.uri(uriBuilder -> uriBuilder.path("movie/" + id + "/credits" ).queryParam("api_key", api_key).build())
 				.retrieve().bodyToMono(HashMap.class).block();
 	}
-
 	public HashMap<String, Object> findImage(int id) {
 		return webClient.get()
 				.uri(uriBuilder -> uriBuilder.path("movie/" + id + "/images" ).queryParam("api_key", api_key).build())
 				.retrieve().bodyToMono(HashMap.class).block();
 	}
-
 	public HashMap<String, Object> findKeywords(int id) {
 		return webClient.get()
 				.uri(uriBuilder -> uriBuilder.path("movie/" + id + "/keywords" ).queryParam("api_key", api_key).build())
 				.retrieve().bodyToMono(HashMap.class).block();
 	}
-
 	public HashMap<String, Object> findRecommendation(int id) {
 		return webClient.get()
 				.uri(uriBuilder -> uriBuilder.path("movie/" + id + "/recommendations" ).queryParam("api_key", api_key).build())
 				.retrieve().bodyToMono(HashMap.class).block();
 	}
-
 	public HashMap<String, Object> findSimilar(int id) {
 		return webClient.get()
 				.uri(uriBuilder -> uriBuilder.path("movie/" + id + "/similar" ).queryParam("api_key", api_key).build())
